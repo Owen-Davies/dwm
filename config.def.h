5fc15ca74f9ae657b4ee717a619c4046e211094d
Owen Davies <odavies@gmail.com>
2020-12-23 12:25:19 +0000

    Added styling for my dwm config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Hack Nerd Font:size=12" };
+static const char dmenufont[]       = "Hack Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "龎", "", "", "", "", "ﰝ" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,8 +28,20 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "st-256color", NULL,    "cava",    1 << 8,      0,           -1 },
+	{ "st-256color", NULL,    "ncmpcpp",    1 << 8,      0,           -1 },
+	{ "Virt-manager",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Virt-manager",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Signal", NULL,    NULL,    1 << 4,      0,           -1 },
+	{ "Microsoft Teams - Preview", NULL,    NULL,    1 << 4,      0,           -1 },
+	{ "Messenger for Desktop", NULL,    NULL,    1 << 4,      0,           -1 },
+	{ "st-256color", NULL,    "Notes",    1 << 3,      0,           -1 },
+	{ "st-256color", NULL,    "Organised",1 << 2,      0,          -1 },
+	{ "st-256color", NULL,    "Code",     1 << 1,      0,           -1 },
+
 };
+
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -56,12 +68,21 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-l", "15", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *vifmcmd[]  = { "st", "-e", "vifmrun", NULL };
+static const char *passmenucmd[]  = { "passmenu", "-i", "-l", "15", NULL };
+static const char *bookmarkscmd[]  = { "demnu-bm", NULL };
+static const char *slockcmd[]  = { "slock", NULL };
 static const char *termcmd[]  = { "st", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY, 	                XK_l, 	   spawn,          {.v = slockcmd } },
+	{ MODKEY|ShiftMask,             XK_p, 	   spawn,          {.v = passmenucmd } },
+	{ MODKEY,                       XK_e, 	   spawn,          {.v = vifmcmd } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {.v = bookmarkscmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
